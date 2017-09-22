3f6c59bcfff82431537542c3b77715d0cad3dfce
Gwenael LARMET <gwenael.larmet-ext@randstad.fr>
2017-09-22 17:21:13 +0200

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule } from '@angular/material';
+
 
 import { AppComponent } from './app.component';
+
+
+import { LibModule } from 'game-of-thrones';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    LibModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
